39a947bdb885803c43df11fcf726fef50f91b720
erickseidmann <erickseidmann@gmail.com>
2023-11-16 13:06:37 -0300

    correção do css e adição de func no js

--- header/footer/mobile-navbar.js
+++ header/footer/mobile-navbar.js
@@ -1,34 +1,44 @@
 class MobileNavbar {
     constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass= "active";
-
-        this.handleClick = this.handleClick.bind(this);
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
     }
-
-    handleClick(){
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
     }
-
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click", this.handleClick);
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
     }
-
-    init(){
-        if(this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
     }
-}
-
-const mobileNavbar = new MobileNavbar(
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
     ".mobile-menu",
     ".nav-list",
     ".nav-list li",
-);
-
-mobileNavbar.init();
+  );
+  mobileNavbar.init();
